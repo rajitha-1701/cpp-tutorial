5ddb79897f10b65f7973a5072b6c9053110f834c
rajitha-1701 <rajitha.harikrishnan@learner.manipal.edu>
2025-02-25 23:05:38 +0530

    Added header inline

--- include/linked_list/link.hpp
+++ include/linked_list/link.hpp
@@ -10,13 +10,42 @@ namespace dsa
     template <typename T>
     class linked_list
     {
-        private:
-            std::shared_ptr<node<T> > head;
-            std::shared_ptr<node<T> > tail;
-        public:
-            linked_list();
-            int add_node(T data);
-            int display();
+    private:
+        std::shared_ptr<node<T>> head;
+        std::shared_ptr<node<T>> tail;
+    
+    public:
+        linked_list() : head(nullptr), tail(nullptr) {}  // Constructor definition
+
+        int add_node(T data)
+        {
+            std::shared_ptr<node<T>> temp = std::make_shared<node<T>>();
+            temp->setData(data);
+            temp->setPointer(nullptr);
+            
+            if (head == nullptr)
+            {
+                head = temp;
+                tail = temp;
+            }
+            else
+            {
+                tail->setPointer(temp);
+                tail = temp;
+            }
+            return 0;
+        }
+
+        int display()
+        {
+            std::shared_ptr<node<T>> temp = head;
+            while (temp != nullptr)
+            {
+                std::cout << temp->getData() << std::endl;
+                temp = temp->getPointer();
+            }
+            return 0;
+        }
     };
 
 } // namespace dsa
